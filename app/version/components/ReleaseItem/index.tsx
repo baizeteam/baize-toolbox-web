d8387c598aaebf69a451170efeadefc9ce26a68b
xiaoyu <xiaoyu@sulg.top>
2024-04-21 14:15:30 +0800

    🐛 修复组件传参问题

--- app/version/components/ReleaseItem/index.tsx
+++ app/version/components/ReleaseItem/index.tsx
@@ -8,7 +8,7 @@ export default function ReleaseItem(props) {
   const { release } = props;
   return (
     <div className="z-20 flex flex-col border border-gray-300 text-left rounded-xl p-8 dark:border-neutral-800 w-full mb-12  last-of-type:mb-0">
-      <ReleaseHeader />
+      <ReleaseHeader release={release} />
       <h4 className="mt-6 font-bold text-4xl text-gray-800 dark:text-neutral-200">
         {release.tag_name}
       </h4>
