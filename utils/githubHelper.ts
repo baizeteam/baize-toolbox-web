7866ecd98dc3c8c9a3559ab2806c9f0e15e264a6
xiaoyu <xiaoyu@sulg.top>
2024-04-28 00:48:31 +0800

    ✨ 移除缓存

--- utils/githubHelper.ts
+++ utils/githubHelper.ts
@@ -1,6 +1,6 @@
 export const repoUrl = `https://api.github.com/repos/${process.env.GITHUB_OWNER}/${process.env.GITHUB_REPO}`;
 export const baseParams = {
-  next: { revalidate: 600 },
+  // next: { revalidate: 600 },
   headers: {
     "Content-Type": "application/json",
     Authorization: `token ${process.env.GITHUB_TOKEN as string}`,
